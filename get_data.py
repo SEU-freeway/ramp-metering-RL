e4611f36c8e7ceccbd9c363becfa3877714b67db
Lenoral <42493335+Lenoral@users.noreply.github.com>
2020-10-05 12:08:13 +0200

    update

--- get_data.py
+++ get_data.py
@@ -28,6 +28,15 @@ path = 'D://Users//LLH//PycharmProjects//BH_rampcontrol//simulation_cfg//'
 os.chdir(path)
 with open(path + 'info_controlled_ramps.json', 'r') as f:
     info = json.load(f)
+
+get_data_interval = 20
+ramp_lanes_num = 4
+merge_area_lane = 5
+discrete_mainstream_arriving_flow = 100
+discrete_density_of_merging_area = 2
+discrete_ramp_queue = 10
+discrete_red_duration = 2
+
 for i in range(1):
     m, r = random_flow(path, average_demand_mainline, average_demand_ramp, 0.2, 0.1)
     mainline_set_flow.extend(m)
@@ -37,8 +46,8 @@ for i in range(1):
     # 运行
     q, r = alinea_control(i, info)
     # 存储数据
-    queue.extend(q[30:])
-    redtime.extend(r[30:])
+    queue.extend(q[int(warmup/get_data_interval):])
+    redtime.extend(r[int(warmup/get_data_interval):])
     # 获取输出文件中的检测器数据
     s, _, f = read_detectorout(detector_list, str(i) + 'detector_out.xml', warmup)
     for d in detector_list:
@@ -46,26 +55,26 @@ for i in range(1):
         flow[d].extend(f[d])
 
     mainline_demand_array = np.repeat(np.array(mainline_set_flow), 15 * 3).reshape(-1, 1)
-    mainline_flow_array = np.vstack((np.array(flow['814 SB']), np.array(flow['813 SB']), np.array(flow['812 SB']),
-                                     np.array(flow['811 SB']), np.array(flow['810 SB']))).T * 3600 / 20 / 4
+    mainline_flow_array = np.vstack((np.array(flow['814 SB']) / 3, np.array(flow['813 SB']) / 3, np.array(flow['812 SB']) / 5,
+                                     np.array(flow['811 SB']) / 4, np.array(flow['810 SB']) / 3)).T * 3600 / get_data_interval
     mainline_speed_array = np.vstack((np.array(speed['814 SB']), np.array(speed['813 SB']), np.array(speed['812 SB']),
                                       np.array(speed['811 SB']), np.array(speed['810 SB']))).T * 3.6
     ramp_demand_array = np.repeat(np.array(ramp_set_flow), 15 * 3).reshape(-1, 1)
     ramp_queue_array = np.array(queue).T.reshape(-1, 1)
-    ramp_flow_array = (np.array(flow['ramp']).T * 3600 / 20 / 4).reshape(-1, 1)
-    ramp_speed_array = (np.array(speed['ramp']).T * 3600 / 20 / 4).reshape(-1, 1)
-    rl_state_array1 = ((np.array(flow['814 SB']) + np.array(flow['813 SB'])) * 3600 / 20 / 4 / 2 / 100).astype(
+    ramp_flow_array = (np.array(flow['ramp']).T * 3600 / 20 / 2).reshape(-1, 1)
+    ramp_speed_array = (np.array(speed['ramp']).T * 3.6).reshape(-1, 1)
+    rl_state_array1 = ((np.array(flow['814 SB'])/3 + np.array(flow['813 SB'])/3) * 3600 / get_data_interval / 2 / discrete_mainstream_arriving_flow).astype(
         int).reshape(-1, 1)
     rl_state_array2 = np.where(np.array(flow['812 SB']) != 0,
-                               np.array(flow['812 SB']) * 3600 / 20 / 4 / (np.array(speed['812 SB']) * 3.6) / 2,
+                               np.array(flow['812 SB']) * 3600 / get_data_interval / 5 / (np.array(speed['812 SB']) * 3.6) / discrete_density_of_merging_area,
                                0).astype(int).reshape(-1, 1)
-    rl_state_array3 = (ramp_queue_array / 10).astype(int).reshape(-1, 1)
-    rl_state_array4 = (np.array(redtime) / 2).astype(int).reshape(-1, 1)
-    rl_action_array = (np.array(redtime) / 2).astype(int).reshape(-1, 1)  # Need to be moved one step forward
+    rl_state_array3 = (ramp_queue_array / discrete_ramp_queue).astype(int).reshape(-1, 1)
+    rl_state_array4 = (np.array(redtime) / discrete_red_duration).astype(int).reshape(-1, 1)
+    rl_action_array = (np.array(redtime) / discrete_red_duration).astype(int).reshape(-1, 1)  # Need to be moved one step forward
     rl_action_array = np.vstack((0, rl_action_array[1:]))
     rl_queue_punish = np.where(ramp_queue_array > 130, 1 - (ramp_queue_array - 120) / 100, 1).reshape(-1, 1)
     rl_reward_array = np.array(flow['810 SB']).T.reshape(-1,
-                                                         1) * 3600 / 20 / 4 * rl_queue_punish  # Need to be moved one step forward
+                                                         1) * 3600 / 20 / 3 * rl_queue_punish  # Need to be moved one step forward
     rl_reward_array = np.vstack((0, rl_reward_array[1:, :]))
     all_data = np.hstack((mainline_demand_array, mainline_flow_array, mainline_speed_array, ramp_demand_array,
                           ramp_queue_array, ramp_flow_array, ramp_speed_array, rl_state_array1, rl_state_array2,
